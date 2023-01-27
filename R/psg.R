8b110a919fbe46b2892356a74514bd4038d6ed30
kskapil08 <88305206+kskapil08@users.noreply.github.com>
2023-01-27 12:04:57 +0530

    Add files via upload

---
+++ R/psg.R
@@ -0,0 +1,14 @@
+#' Basic Function
+#' @export
+#' @param x numeric
+
+
+
+basic_function = function(v){
+  data.frame(
+    min = min(v),
+    max  = max(v),
+    range = max(v) - min(v),
+    std_dev = sd(v)
+  )
+}
